ca17676965a2d2917c4693acc5fa578eeea58183
saiprasanthg <saiprasanth.g5@gmail.com>
2017-08-20 17:35:18 +0530

    added size of int

---
+++ sizeoffloat.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<float.h>
+int main()
+{
+	//size of float//
+	printf("enter the size of float: %lu \n",sizeof(float));
+	printf("max size of float is:%E \n",FLT_MAX);
+	//max size of float//
+	printf("min size of float is:%E \n",FLT_MIN);
+	//min size of float//
+}
