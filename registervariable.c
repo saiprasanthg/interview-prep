9fbbce6544d4765912343ce3814f655360f8b14c
saiprasanthg <saiprasanth.g5@gmail.com>
2017-08-20 22:21:09 +0530

    use of register variable

---
+++ registervariable.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+void test();   //Function declaration 
+ 
+int main()
+{
+register int number; //register variable which generate a random value
+printf("the value is %d\n", number);
+ 
+ return 0;
+}
