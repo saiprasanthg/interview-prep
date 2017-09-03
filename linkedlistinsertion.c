098bdb60caa57c26318cdec11e0b7294869c026d
saiprasanthg <saiprasanth.g5@gmail.com>
2017-09-03 12:22:59 +0530

    linked list insertion

---
+++ linkedlistinsertion.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{ // here struct is used for different data types in linked list//
+	int data;
+	struct node *next;
+};
+void insertNode(int v);// function declaration 
+void print();
+struct node* head;
+void insertNode(int v)
+{
+struct node* temp=(struct node*)malloc(sizeof(struct node)); // node creation using malloc //
+temp->data=v;			
+temp->next=head;	// so adress of head would be now the address of temp->next
+head=temp;			 
+}
+
+void print()  // to print the nodes //
+{
+	struct node* temp=head;
+		printf("list");
+	while(temp!=NULL) // for traversal in the linked list //
+	{
+		printf("%d",temp->data);
+		temp=temp->next;
+    }
+	printf("\n");
+}
+int main()
+{
+	printf("no of numbers");
+	int n,i,v; //n is the number of nodes
+	scanf("%d",&n);
+	for(i=0;i<n;i++) 
+	{
+	printf("enter the numbers:");
+	scanf("%d",&v);
+	insertNode(v);
+	print();
+	}
+	
+}
